25487ed7bfe0c5a16f4369a9fb611033e7894d79
asrahim88 <tasmimhasan1997@gmail.com>
2025-04-25 12:18:31 +0600

    2nd program commit

---
+++ p_2_input_output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter a and b numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("The value of a is :%d\n", a);
+    printf("The values of b is :%d", b);
+    return 0;
+}
