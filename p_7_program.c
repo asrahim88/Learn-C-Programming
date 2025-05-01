da04a648945ddaeffe76b0231907805753a6e0fb
asrahim88 <tasmimhasan1997@gmail.com>
2025-05-01 14:35:39 +0600

    practice probelm-1

---
+++ p_7_program.c
@@ -0,0 +1,12 @@
+/*
+    Write a C program to show output like below:
+
+    Hi there /\.    I hope you are doing great and practicing well.
+    Now this is a simple task  you need to do.      Just care for the indentation.
+    Show 100% of sincerity.
+*/
+#include<stdio.h>
+int main(){
+    printf("Hi there /\\.\tI hope you are doing great and practicing well.\nNow this is a simple task  you need to do.\t\tJust care for the indentation.\nShow 100%% of sincerity.");
+    return 0;
+}
