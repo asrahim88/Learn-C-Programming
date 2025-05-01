4e7a291641f2ba0133e45f9097a2e72ee1ff6b9d
asrahim88 <tasmimhasan1997@gmail.com>
2025-05-01 14:40:04 +0600

    practice program-2

---
+++ p_8_program.c
@@ -0,0 +1,13 @@
+/*
+    Write a C program to declare two integer variables and set values 10 and 20 respectively. Now print the second number and then the first number.
+Sample Input:                               Sample Output:
+No Input                                        20 10
+*/
+#include<stdio.h>
+int main(){
+    int a, b;
+    a = 10;
+    b = 20;
+    printf("%d %d", b,a);
+    return 0;
+}
