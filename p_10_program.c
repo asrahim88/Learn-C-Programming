76899d138bfb301ceac5bc0b994a9c19d04cb350
asrahim88 <tasmimhasan1997@gmail.com>
2025-05-02 10:11:46 +0600

    practice program_4

---
+++ p_10_program.c
@@ -0,0 +1,26 @@
+/*
+    Write a C program to take two positive integers as input and show the summation, subtraction and
+    multiplication of those two numbers.
+    Sample Input:                           Sample Output:
+    5 2                                         7 3 10
+*/
+
+#include<stdio.h>
+int main(){
+    int a,b,summation, subtraction, multiplication;
+    printf("Enter two positive number: ");
+    scanf("%d %d", &a, &b);
+    if(a>0 & b>0) {
+        summation = a+b;
+        subtraction = a-b;
+        multiplication = a*b;
+        printf("The summation is : %d\n", summation);
+        printf("The subtraction is : %d\n", subtraction);
+        printf("The multiplication is : %d\n", multiplication);
+    }
+    else {
+        printf("please enter the two positive number");
+    }
+    return 0;
+}
+
