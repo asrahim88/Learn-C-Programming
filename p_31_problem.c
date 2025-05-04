a5146400a4fce40a2da321dd320428ebbd83a429
asrahim88 <asrahim162@gmail.com>
2025-05-04 21:12:16 +0600

    problem solve

---
+++ p_31_problem.c
@@ -0,0 +1,46 @@
+/*
+    You need to take 4 distinct integer as input. Print the largest and smallest among them. Distinct means all of them are separate integers. None of them is equal to any other of them. 
+
+    Sample Input: 
+    123 435 34 612 
+    Sample Output: 
+    Largest = 612 
+    Smallest = 34 
+
+*/
+#include<stdio.h>
+int main(){
+    int a, b, c, d;
+    printf("Please Enter the 4 Distinct Numbers: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    if (a>b && a >c && a>d)
+    {
+        printf("Largest = %d", a);
+    } else if (b>a && b>c && b >d)
+    {
+        printf("Largest = %d", b);
+        
+    } else if (c>a && c>b && c >d)
+    {
+        printf("Largest = %d", c);
+        
+    }else {
+        printf("Largest = %d", d);
+    }
+    printf("\n");
+    if (a<b && a <c && a<d)
+    {
+        printf("Smallest = %d", a);
+    } else if (b<a && b<c && b <d)
+    {
+        printf("Smallest = %d", b);
+        
+    } else if (c<a && c<b && c <d)
+    {
+        printf("Smallest = %d", c);
+        
+    }else {
+        printf("Smallest = %d", d);
+    }
+}
