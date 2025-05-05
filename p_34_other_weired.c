d8ebbc4859c9a62c172c1e34c1443b499067ccf4
asrahim88 <asrahim162@gmail.com>
2025-05-05 10:16:45 +0600

    weired algorithm

--- p_34_other_weired.c
+++ p_34_other_weired.c
@@ -1,3 +1,12 @@
+/*
+    Let us create a new version of weird algorithm. You will be given an integer, n as input. If n is even, divide it by two. If n is odd, then subtract 1 from n. Eventually it will end at 1. So print the whole sequence. For example, 
+    Sample Input: 
+    123 
+    Sample Output: 
+    123, 122, 61, 60, 30, 15, 14, 7, 6, 3, 2, 1 
+    Follow the whole output format(each numbers separated by commas). 
+
+*/
 #include<stdio.h>
 int main(){
     int n;
