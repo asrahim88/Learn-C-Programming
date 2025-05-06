f941c47f920651cae2b540f0bc4c8d055368c220
asrahim88 <asrahim162@gmail.com>
2025-05-06 11:46:03 +0600

    updated

--- p_50_pattern6.c
+++ p_50_pattern6.c
@@ -3,11 +3,11 @@ int main(){
     int r;
     printf("Enter your row number: ");   
     scanf("%d", &r);
-    for (int i = 64; i <r+64; i++)
+    for (int i = 1; i < r; i++)
     {
-        for (int j = 64; j < i; j++)
+        for (int j = 1; j < i; j++)
         {
-            printf("%c ", i);
+            printf("%c ", i+63);
         }
         printf("\n");
     }
