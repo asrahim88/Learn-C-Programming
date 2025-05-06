3f0944e2a6200f641d2abd50aac933f4a37a3e42
asrahim88 <asrahim162@gmail.com>
2025-05-07 05:59:41 +0600

    pattern22.c

---
+++ p_64_pattern22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter your row number: ");   
+    scanf("%d", &row);
+
+    for (int i = 1; i <=row; i++)
+    {
+        for (int j = 1; j <=row-i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <=i; j++)
+        {
+            printf("%d", j%2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
