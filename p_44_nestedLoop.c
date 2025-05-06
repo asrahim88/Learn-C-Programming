967bb29d6f5ccfdec8a1a7613cd2e0ecfa5e063a
asrahim88 <asrahim162@gmail.com>
2025-05-06 10:43:46 +0600

    nested loop using for loop

---
+++ p_44_nestedLoop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter your row number: ");   
+    scanf("%d", &r);
+    printf("Enter your column number: ");   
+    scanf("%d", &c);
+    
+    for (int i = 0; i <r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
