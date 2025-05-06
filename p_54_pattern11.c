04c8f8188b24f64e985d85495aa6a1a48560ec4a
asrahim88 <asrahim162@gmail.com>
2025-05-06 12:36:44 +0600

    pattern11

---
+++ p_54_pattern11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter your row number: ");   
+    scanf("%d", &row);
+
+    for (int i = row; i >=1; i--)
+    {
+        for (int j = 1; j<=i; j++)
+        {
+            printf("%d ", i%2);
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
