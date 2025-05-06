f648e5e23bf2f5aa878df3f3925d3cb8df7df2e9
asrahim88 <asrahim162@gmail.com>
2025-05-06 12:50:19 +0600

    pattern13

---
+++ p_56_pattern13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter your row number: ");   
+    scanf("%d", &row);
+   
+    for (int i = row; i >=1; i--)
+    {
+        for (int j = 1; j<=i; j++)
+        {
+            printf("%c ", i+64);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
