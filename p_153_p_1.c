e97fe2c91be5ae98b85ed6eb67bb401275e1e322
asrahim88 <asrahim162@gmail.com>
2025-09-21 16:39:15 +0600

    practice_1

---
+++ p_153_p_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+
+    scanf("%d %d", &num1, &num2);
+
+    int sum = num1 + num2;
+    printf("The sum of %d and %d is %d", num1, num2, sum);
+    return 0;
+}
