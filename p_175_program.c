c7c9c7c85e54ae7b39333088649d42a03c207353
asrahim88 <asrahim162@gmail.com>
2025-09-24 19:37:50 +0600

    Add two numbers.

---
+++ p_175_program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int N, M;
+    scanf("%d %d", &N, &M);
+    
+    printf("%d", N+M);
+    return 0;
+}
