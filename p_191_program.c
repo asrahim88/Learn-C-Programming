bcf162b4541eab30e54d1b1c671a4245e1379ccd
asrahim88 <asrahim162@gmail.com>
2025-09-29 15:05:29 +0600

    program191

---
+++ p_191_program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 2; i <=n; i++)
+    {
+        /* code */
+        if (i%2==0)
+        {
+            /* code */
+            printf("%d ", i);
+        }
+        
+    }
+    
+    return 0;
+}
