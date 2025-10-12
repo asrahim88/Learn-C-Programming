b34c1fa4f14dc445199d54bfcc1ce910fd4e82ae
asrahim88 <asrahim162@gmail.com>
2025-10-12 19:13:16 +0600

    program222

---
+++ p_222_program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    if(x<5) {
+        printf("hello");
+    }
+    if(x<=10) {
+        printf("hi");
+    }else {
+        printf("no");
+    }
+    return 0;
+}
