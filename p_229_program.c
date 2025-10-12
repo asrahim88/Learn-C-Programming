ccf60289ae9d8508c2b3d1b3dc2ed3a85296ce3e
asrahim88 <asrahim162@gmail.com>
2025-10-12 19:39:36 +0600

    program229

---
+++ p_229_program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    int y = 4;
+    
+    if(x>=y && x < y + 4) {
+        if (y < 5) {
+            printf("Hi ");
+        }
+        if (y <10){
+            printf("Hi ");
+        }
+    }else {
+        printf("Hello ");
+    }
+    
+    return 0;
+}
