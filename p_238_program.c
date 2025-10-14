2af457dffdc58e2a0203d615f1c616ba3dd71ad6
asrahim88 <asrahim162@gmail.com>
2025-10-14 17:35:14 +0600

    program238

---
+++ p_238_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    for (int i = 0; i <=10; i++)
+    {
+        /* code */
+        printf("Hi ");
+        if (i>2)
+        {
+            break;
+        }
+        
+    }
+    
+    return 0;
+}
