d81eb5ff202ba930bb3503b0258e9db66605fad8
asrahim88 <asrahim162@gmail.com>
2025-10-14 22:33:17 +0600

    factorial solved

--- p_245_factorial.c
+++ p_245_factorial.c
@@ -3,17 +3,20 @@ int main() {
     int N;
     scanf("%d", &N);
 
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i <=N; i++)
     {
-        int x, fact = 1;
-        scanf("%d", &x);
-                
-        for (int i = 1; i <=x; i++)
+        long long int x, fact = 1;
+        scanf("%lld", &x);
+
+        if(x == 0 ) {
+            printf("1\n");
+            continue;
+        }
+        for (long long int i = 1; i <=x; i++)
         {
             fact = fact * i;
         }
-        printf("%d\n", fact);
+        printf("%lld\n", fact);
     }
-    
     return 0;
 }
