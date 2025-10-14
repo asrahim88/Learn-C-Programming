172e50b5107205f839d23584bfeb7937950c2486
asrahim88 <asrahim162@gmail.com>
2025-10-14 18:15:26 +0600

    Program241

---
+++ p_241_program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    for (int i = -4; i >=0; i++)
+    {
+        for (int j = 5; j < 2; j++)
+        {
+            printf("Hi ");
+        }
+        
+    }
+    
+    return 0;
+}
