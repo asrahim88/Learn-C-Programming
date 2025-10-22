4690a38e7ec0c1733a048e9a55c2a040050742d9
asrahim88 <asrahim162@gmail.com>
2025-10-22 23:04:56 +0600

    string practice

---
+++ p_254_program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    char ch[100];
+    scanf("%s", ch);
+
+    int count = 0; 
+    for (int i = 0; ch[i] != '\0'; i++)
+    {
+        count++;
+    }
+    printf("%d", count);
+    return 0;
+}
