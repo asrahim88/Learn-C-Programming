cb70126f1f56c73e6cb16f07b28f5e4a7a363f53
asrahim88 <asrahim162@gmail.com>
2025-10-23 12:09:38 +0600

    one string copy to other string

---
+++ p_261_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str1[101], str2[101];
+    scanf("%s %s", str1, str2);
+
+    int length = strlen(str2);
+    
+    for(int i = 0; i<=length; i++){
+        str1[i] = str2[i];
+    }
+
+    printf("%s %s", str1, str2);
+    return 0;
+}
