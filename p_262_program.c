90f6b4e81b23383f889776425aface24d62c5355
asrahim88 <asrahim162@gmail.com>
2025-10-23 12:18:02 +0600

    Copy string by the built in function

---
+++ p_262_program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str1[101], str2[101];
+
+    scanf("%s %s", str1, str2);
+    strcopy(str1, str2);
+
+    printf("%s %s", str1, str2);
+    return 0;
+}
