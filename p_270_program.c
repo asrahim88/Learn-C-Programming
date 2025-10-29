5eb167c8f0acd70305dfc2db3a77ccc5c551f9f7
asrahim88 <asrahim162@gmail.com>
2025-10-29 19:29:07 +0600

    Multiplication

---
+++ p_270_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    long long int A, B;
+    scanf("%lld %lld", &A, &B);
+    printf("%lld", A*B);
+    return 0;
+}
