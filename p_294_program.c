76ea939798d8edce5aafc6a1f7c17716de08357d
asrahim88 <asrahim162@gmail.com>
2025-11-02 17:00:53 +0600

    program294

---
+++ p_294_program.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int space = n-1;
+    int number = 1;
+    for(int i = 1; i<=n; i++) {
+        for(int j=1; j<=space; j++) {
+            printf(" ");
+        }
+        for(int j=1;j<=number; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+        space--;
+        number++;
+    }
+    return 0;
+}
