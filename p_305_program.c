3090e0b6c3df9920333b45c603dca69650c47e35
asrahim88 <asrahim162@gmail.com>
2025-11-04 14:40:47 +0600

    program305

---
+++ p_305_program.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int space = N-1;
+    int star = 1;
+    for(int i = 1; i<=N; i++) {
+        for(int j = 1; j<=space; j++) {
+            printf(" ");
+        }
+        for(int j=1; j<=star; j++) {
+            printf("*");
+        }
+        printf("\n");
+        space--;
+        star+=2;
+    }
+    return 0;
+}
