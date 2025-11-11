dc6b4eae16df949ef645d4b850d81096053b66fc
asrahim88 <asrahim162@gmail.com>
2025-11-11 08:38:40 +0600

    program347

---
+++ p_347_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printNumber(int i, int n)
+ {
+    if(i == n+1){
+        return;
+    }
+    printf("%d\n", i);
+    printNumber(i+1, n);
+ }
+int main() {
+    int n;
+    scanf("%d", &n);
+    printNumber(1, n);
+    return 0;
+}
