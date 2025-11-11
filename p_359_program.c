23422b1fec3343d36017ab2bb66341e6faf28c10
asrahim88 <asrahim162@gmail.com>
2025-11-11 17:18:27 +0600

    program359

---
+++ p_359_program.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int aray[r][c];
+
+    for(int i = 0; i<r; i++) {
+        for(int j = 0; j<c; j++) {
+            scanf("%d", &aray[i][j]);
+        }
+    }
+
+    int flag = 0;
+
+    if(r == c) {
+        for(int i =0; i<r; i++) {
+            for(int j = 0; j<c; j++) {
+                if(i !=j && aray[i][j] !=0) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag == 0) {
+            printf("scaler matrix");
+        }else {
+            printf("not scaler matrix.");
+        }
+    }else {
+        printf("not scaler matrix.");
+    }
+    return 0;
+}
